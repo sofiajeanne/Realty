d175d79bcb78d0a055e82138e344e894b13e8d14
sofiajeanne <sofia.rogg@gmail.com>
2016-06-13 22:39:49 -0400

    add main pages text content, image, and links

---
+++ images/HTML-Grover-Image.php
@@ -0,0 +1,225 @@
+<!-- 
+
+SITE DESIGNED AND CODED BY: JONATHAN GROVER - http://jonathangrover.com/
+
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Jonathan Grover - Artist</title>
+	<META NAME="description" content="Jonathan Grover - The artwork and vitae of Jonathan Grover.">
+	<META NAME="keywords" content="jon, jonathan, grover, john, design, web, graphic, web design, portfolio, work, artist, illustration, photography, multimedia, branding, identity, logos, artwork, teaching, teacher, San Francisco, based, sfai, san francisco art institute, interactive, media, technology">
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<meta name="verify-v1" content="hKeqO67Ge/cDKmFu1RTliKhwKc3PzDPHmedoqgpmVgQ=" />
+	<link href="css/site.css" rel="stylesheet" type="text/css">
+    <link href="css/nav.css" rel="stylesheet" type="text/css">
+	<meta name="Owner" content="Jonathan Grover">
+	<meta name="robots" content="index,follow">
+	<meta name="Googlebot" content="index,follow">
+    <meta name="Googlebot-Image" content="index,follow">
+    <script src="scripts/dynamic_iframe.js" type="text/javascript"></script>
+</head>
+
+	<body>
+    
+      <table class="main">
+        <tr>
+          <td class="top_banner">&nbsp;</td>
+        </tr>
+          <tr>
+            <td class="mid_spacer">&nbsp;</td>
+        </tr>
+          <tr>
+            <td class="content">
+            	<table class="divider">
+                	<tr>
+                		<td class="left_nav">
+                        
+							<!-- Nav Code Starts Here -->
+
+<div class="menulink_1">
+		<a href="#"onclick="d=document.getElementById('nest1');d.style.display=d.style.display=='none'?'block':'none'; return false">Artwork</a>
+	</div>
+    	<div id="nest1" style="display:none">
+        
+        <!-- 2010 -->
+         <div class="menulink_4">
+			<a href="#"onclick="d=document.getElementById('nest13');d.style.display=d.style.display=='none'?'block':'none'; return false">2010</a><br />
+    		</div>
+                <div id="nest13" style="display:none" class="menulink_2">
+                
+                 <!-- Bridge -->
+                <a href="#" onclick="d=document.getElementById('nest14');d.style.display=d.style.display=='none'?'block':'none'; return false">Bridge</a><br>
+                    <div id="nest14" style="display:none" class="menulink_3">
+                    	<a href="selected_work/bridge/video2.php" target="innercontent">video</a><br>
+                        <a href="selected_work/bridge/video.php" target="innercontent">video 2</a><br>
+                        <a href="selected_work/bridge/image.php" target="innercontent">image</a><br>
+                        <a href="selected_work/bridge/sound.php" target="innercontent">sound</a><br>
+                    </div>
+                
+                </div>
+        
+        <!-- 2009 -->
+        <div class="menulink_4">
+			<a href="#"onclick="d=document.getElementById('nest11');d.style.display=d.style.display=='none'?'block':'none'; return false">2009</a><br />
+    		</div>
+                <div id="nest11" style="display:none" class="menulink_2">
+        	
+        
+            <!-- Chimes -->
+                <a href="#" onclick="d=document.getElementById('nest3');d.style.display=d.style.display=='none'?'block':'none'; return false">Chimes</a><br>
+                    <div id="nest3" style="display:none" class="menulink_3">
+                        <a href="selected_work/chimes/video.php" target="innercontent">video</a><br>
+                        <a href="selected_work/chimes/image.php" target="innercontent">image</a><br>
+                        <a href="selected_work/chimes/sound.php" target="innercontent">sound</a><br>
+                    </div>
+           
+            <!-- Clotheslines //
+                <a href="#" onclick="d=document.getElementById('nest5');d.style.display=d.style.display=='none'?'block':'none'; return false">Clothesline</a><br>
+                    <div id="nest5" style="display:none" class="menulink_3">
+                        <a href="selected_work/clotheslines/video.php" target="innercontent">video</a><br>
+                        <a href="selected_work/clotheslines/image.php" target="innercontent">image</a><br>
+                    </div>   -->
+            
+            <!-- Table no.1 //
+                <a href="#" onclick="d=document.getElementById('nest4');d.style.display=d.style.display=='none'?'block':'none'; return false">Table</a><br>
+                    <div id="nest4" style="display:none" class="menulink_3">
+                        <a href="selected_work/undertables/video.php" target="innercontent">video</a><br>
+                        <a href="selected_work/undertables/image.php" target="innercontent">image</a><br>
+                    </div> -->
+            
+            <!-- Shadows -->
+                <a href="#" onclick="d=document.getElementById('nest2');d.style.display=d.style.display=='none'?'block':'none'; return false">Shadow</a><br>
+                    <div id="nest2" style="display:none" class="menulink_3">
+                        <a href="selected_work/shadows/video.php" target="innercontent">video</a><br>
+                        <a href="selected_work/shadows/image.php" target="innercontent">image</a><br>
+                    </div>
+            
+     				</div>
+             
+         <!-- 2008 -->
+         <div class="menulink_4">
+			<a href="#"onclick="d=document.getElementById('nest12');d.style.display=d.style.display=='none'?'block':'none'; return false">2008</a><br />
+            </div>
+    			<div id="nest12" style="display:none" class="menulink_2">        
+                    
+            
+            <!-- Red Push Pull //
+            <a href="#" onclick="d=document.getElementById('nest6');d.style.display=d.style.display=='none'?'block':'none'; return false">Red Push Pull</a><br>
+                    <div id="nest6" style="display:none" class="menulink_3">
+                        <a href="selected_work/redpushpull/image.php" target="innercontent">image</a><br>
+                    </div> -->
+            
+            <!-- Ants -->
+                <a href="#" onclick="d=document.getElementById('nest8');d.style.display=d.style.display=='none'?'block':'none'; return false">Ants</a><br>
+                    <div id="nest8" style="display:none" class="menulink_3">
+                        <a href="selected_work/ants/video.php" target="innercontent">video</a><br>
+                        <a href="selected_work/ants/image.php" target="innercontent">image</a><br>
+                    </div>
+             
+        		</div>
+                
+      		</div>
+                    
+<div class="menulink_1">
+		<a href="exhibitions/index.php" target="innercontent">Exhibitions</a>
+	</div>
+<div class="menulink_1">
+		<a href="bio_statement/index.php" target="innercontent">Bio & Statement</a>
+	</div>
+<div class="menulink_1">
+		<a href="resume_cv/index.php" target="innercontent">CV</a>
+	</div>
+<div class="menulink_1">
+		<a href="bibliography_media/index.php" target="innercontent">Press</a>
+	</div>
+    
+    <!-- // hide press kit link now access via press link above //
+    <div class="menulink_1b">
+		<a href="presskit/index.php" target="innercontent">Press Kit</a>
+	</div>
+    -->
+    
+<div class="menulink_1">
+		<a href="about/index.php" target="innercontent">About</a>
+	</div>
+<div class="menulink_1">
+		<a href="contact/index.php" target="innercontent">Contact</a>
+	</div>
+<div class="menulink_1">
+		<a href="links/index.php" target="innercontent">Links</a>
+	</div>
+<div class="menulink_1">
+		<a href="about/news.php" target="innercontent">Follow</a>
+	</div>
+    <br />
+    
+<!-- TEACHING -->
+
+<div class="menulink_1c">
+		<!-- <a href="#"onclick="d=document.getElementById('nest15');d.style.display=d.style.display=='none'?'block':'none'; return false"> --><a href="http://edu.jonathangrover.com" target="_blank">Teaching</a>
+	</div>
+    
+    	<div id="nest15" style="display:none" class="menulink_2">
+                    <a href="#"onclick="d=document.getElementById('nest16');d.style.display=d.style.display=='none'?'block':'none'; return false">Student Resources</a><br />
+        		<div id="nest16" style="display:none"class="menulink_3">
+                    <a href="teaching/students/cwd/index.htm" target="_blank">Creative Website Design</a><br>
+                    <a href="teaching/students/max/index.htm" target="_blank">Max/Msp/Jitter</a><br>
+                    <!-- <a href="cwd/2009/index2.htm" target="_blank">CWD (Summer 2009)</a><br> -->
+                </div>
+
+                    <a href="#"onclick="d=document.getElementById('nest17');d.style.display=d.style.display=='none'?'block':'none'; return false">Portfolio</a>
+        		<div id="nest17" style="display:none"class="menulink_3">
+                	<a href="teaching/portfolio/course_descriptions.html" target="innercontent">Course Descriptions</a><br>
+                    <a href="teaching/portfolio/sample_syllabi.html" target="innercontent">Sample Syllabi</a><br>
+                    <!-- <a href="teaching/index.htm" target="innercontent">Handouts</a><br> -->
+                    <a href="teaching/portfolio/student_work/image.php" target="innercontent">Student Work</a><br>
+                    <a href="teaching/portfolio/evaluations.html" target="innercontent">Evaluations</a><br>
+                    <a href="teaching/portfolio/teaching_philosophy.html" target="innercontent">Teaching Philosophy</a><br>
+                    <!-- <a href="teaching/portfolio/diversity_statement.html" target="innercontent">Diversity Statement</a><br> -->
+                    <a href="teaching/portfolio/transcripts.html" target="innercontent">Transcripts</a><br>
+                    <a href="teaching/portfolio/letters_of_recommendation.html" target="innercontent">Recommendations</a><br>
+                </div>
+                
+        </div>  
+                            
+                        </td>
+                        <td class="page_content">
+                        
+                        <!-- begin iframe -->
+                        
+           				  <iframe name="innercontent" id="innercontent" src="about/index.php" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0" style="overflow:visible; width:100%; display:none"></iframe>
+                        
+                        <!-- end iframe -->
+                        
+                        </td>
+               	  </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td class="footer">&nbsp;</td>
+        </tr>
+      </table>
+      
+<!-- start google analytics code -->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-3839866-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+        
+<!-- end google analytics code -->
+
+      
+</body>
+
+</html>
